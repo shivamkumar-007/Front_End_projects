b7cedc93a4e889085a6e611b37834e979f636d87
shivam_kumar <shivamkumar9891520@gmail.com>
2023-09-13 23:45:18 +0530

    Deleted commented code in file multiple_card_DOM'
    q
    wq
    
    git add .
    '
    '

--- Beginner_projects/multiple_card_DOM/index.js
+++ Beginner_projects/multiple_card_DOM/index.js
@@ -1,28 +1,4 @@
-//  var arr=[1,-2,-3,2,-65,-5676,,6,-34,-4,3,34,-43,4,-5,656,345];
 
-// // var arr2 =
-// arr.forEach(function(val,index) {
-//     if(val<0){
-//         arr.splice(index,1);
-//     }
-    
-// });
-// // var count=0;
-// //  for(let i=0;i<arr.length;i++){
-// //         if(arr[i]<0){
-// //             count++;
-// //         }
-
-// //    }
-
-// //    for(var j=0;j<count;j++){
-    // //     for(var i=0;i<arr.length;i++){
-    // //      if(arr[i]<0){
-    // //         arr.splice(i,1);
-// //         break;
-// //     }
-// //    }
-// // }
 
 var details=[
     {name:"Harsh",status:"Strangers",img:"https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"},
@@ -46,7 +22,6 @@ details.forEach(function(val,index){
 document.querySelector("main").innerHTML=card;
 
 document.querySelector("main");
-main.addEventL
 
 
 
